92d7dba703bced7f6533e91ecd96049ee1b99436
Enrique <enriquechristian2008@gmail.com>
2023-06-09 01:42:17 -0500

    Reasons added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import Banner from './scenes/Banner'
 import Hero_Section from './scenes/Hero_Section'
+import Reasons from './scenes/Reasons'
 
 function App() {
 
   return (
     <>
       <Banner/>   
-      <Hero_Section/> 
+      <Hero_Section/>
+       <Reasons/>
     </>
   )
 }
